a27951d509afb09b5f539fe6a5ac5ffb73483f68
unknown <Stokeskristen817@gmail.com>
2018-11-14 16:38:38 -0500

    another push

--- src/home/Navbar.js
+++ src/home/Navbar.js
@@ -31,7 +31,7 @@ class SiteBar extends React.Component {
                 <Collapse isOpen={this.state.isOpen} navbar>
                     <Nav className="ml-auto" navbar>
                         <NavItem>
-                            <Button onClick={() => this.props.clickLogout()}>Logout</Button>
+                            <Button onClick={() => this.props.clickLogout()} outline color="info">Logout</Button>
                         </NavItem>
                     </Nav>
                 </Collapse>
